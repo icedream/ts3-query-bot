8b9481ee0ab43f5df3d1ccdafab668d3e72358cd
icedream <icedream2k9@die-optimisten.net>
2013-06-23 22:07:07 +0200

    Let's add a sound plugin (only functional for me).

---
+++ src/plugins/soundplayerplugin/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace TS3Query.Plugins
+{
+    [PluginMetadata("Sound plugin", "Icedream", Description="Allows playing sounds via the bot (via a virtual audio cable).")]
+    public class SoundPlugin
+    {
+        Process p = null;
+
+        public SoundPlugin()
+        {
+        }
+
+        [PluginCommand("play", Description = "Plays a sound or a video.", TargetMode = TS3MessageTargetMode.Private)]
+        public void Play([PluginCommandParameter("url", Description="The URL.")]string url)
+        {
+            if (p != null && !p.HasExited)
+                return;
+
+            p = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "VideoLAN", "VLC", "vlc.exe"),
+                    Arguments = string.Format("-I rc -vvv --aout=aout_directx --play-and-exit --directx-audio-device=\"Line 4 (TeamSpeak Out) (Virtual Audio Cable)\" {0}", url),
+                    UseShellExecute = false,
+                    RedirectStandardError = false,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = false
+                }
+            };
+            p.WaitForExit();
+        }
+    }
+}
---
+++ src/plugins/soundplayerplugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Sound Plugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Icedream")]
+[assembly: AssemblyProduct("TS3 Client Query Bot")]
+[assembly: AssemblyCopyright("© 2013 Icedream")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("4e7b4fe7-6ab1-49e2-8ee8-e7eed64f149c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1")]
