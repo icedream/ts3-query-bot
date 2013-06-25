9de8210bfd84e8c57c1ea37bfe728a00c89f661b
icedream <icedream2k9@die-optimisten.net>
2013-06-25 05:50:12 +0200

    A bugfix and some new additions.
    - Bugfix: Parsing response parameters includes applying values from previous data map.
    - Added: GenerateObject<T>() to apply parameters to a fresh new instance of T, which expects the parameter names to be available as fields.
    - Added: Timestamp of response, will be used for queue implementation.

--- src/bot/TS3QueryResponse.cs
+++ src/bot/TS3QueryResponse.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
 using System.Linq;
 using System.Text;
 
@@ -9,22 +13,65 @@ namespace TS3Query
 
     public class TS3QueryResponse
     {
-        public Dictionary<string, string> Parameters { get; private set; }
+        internal DateTime Timestamp = DateTime.Now;
+
+        public List<Dictionary<string, string>> Parameters { get; private set; }
         public string Name { get; private set; }
+
+        internal IEnumerable<T> GenerateObject<T>()
+        {
+            foreach (var part in Parameters)
+            {
+                // Generate an instance of T
+                T obj = (T)Activator.CreateInstance(typeof(T));
+
+                // Fill it with details from this parameter part
+                foreach (var item in part)
+                {
+                    // Check if a field for this detail exists
+                    var field = obj.GetType().GetField(item.Key);
+                    if (field == null) // Field does not exist
+                        continue; // Skip
+
+                    // Convert parameter string to field type
+                    var tc = TypeDescriptor.GetConverter(field.FieldType);
+                    try
+                    {
+                        var fobj = tc.ConvertFromString(item.Value);
+                        field.SetValue(obj, item.Value); // Inject object into instance of T
+                    }
+                    catch { { } }
+                }
+
+                yield return obj;
+            }
+        }
 
         internal TS3QueryResponse(string rawLine)
         {
             var rawLineSplit = new Queue<string>(rawLine.Split(' '));
 
-            Name = rawLineSplit.Dequeue();
-            Parameters = new Dictionary<string, string>();
+            if (!rawLineSplit.First().Contains('='))
+                Name = TS3QueryTextFilter.DecodeText(rawLineSplit.Dequeue());
 
-            while (rawLineSplit.Any())
+            rawLine = string.Join(" ", rawLineSplit);
+            Parameters = rawLine.Split('|').Select(i => i.Split(' ').Select(j => { var k = j.Split('='); return new KeyValuePair<string, string>(TS3QueryTextFilter.DecodeText(k[0]), k.Length > 1 ? TS3QueryTextFilter.DecodeText(k[1]) : null); }).ToDictionary(k => k.Key, k => k.Value)).ToList();
+
+            // Fill up parameters as the server expects us to apply values from (n-1)th instance to nth instance
+            // automatically. Again, a weird way of parsing things... but okay...
+            if (Parameters.Count > 1)
             {
-                var rawArgSplit = rawLineSplit.Dequeue().Split('=');
-                var rawArgName = TS3QueryTextFilter.DecodeText(rawArgSplit.First());
-                var rawArgValue = TS3QueryTextFilter.DecodeText(string.Join("=", rawArgSplit.Skip(1)));
-                Parameters.Add(rawArgName, rawArgValue);
+                for (int i = 1; i < Parameters.Count; i++)
+                {
+                    var previousInstance = Parameters[i - 1];
+                    var thisInstance = Parameters[i];
+
+                    foreach (var item in previousInstance)
+                    {
+                        if (!thisInstance.ContainsKey(item.Key))
+                            thisInstance.Add(item.Key, item.Value); // Apply value from previous instance
+                    }
+                }
             }
         }
 
@@ -32,12 +79,7 @@ namespace TS3Query
         {
             StringBuilder sb = new StringBuilder();
             sb.Append(TS3QueryTextFilter.EncodeText(Name));
-
-            foreach (var arg in Parameters)
-            {
-                sb.AppendFormat(" {0}={1}", TS3QueryTextFilter.EncodeText(arg.Key), TS3QueryTextFilter.EncodeText(arg.Value));
-            }
-
+            sb.Append(string.Join("|", Parameters.Select(pg => string.Format(" ", pg.Select(p => TS3QueryTextFilter.EncodeText(p.Key) + "=" + TS3QueryTextFilter.EncodeText(p.Value))))));
             return sb.ToString();
         }
     }
