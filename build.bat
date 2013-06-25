2cd741c0f79f5718d30e379b5085416e852b182b
icedream <icedream2k9@die-optimisten.net>
2013-06-25 04:43:50 +0200

    Added some status lines to the build batch script.

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@
 path %path%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 path %path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 
+echo Building debug binaries...
 msbuild Bot.sln /m /nologo /maxcpucount /v:m /p:Configuration="Debug"
+echo Building release binaries...
 msbuild Bot.sln /m /nologo /maxcpucount /v:m /p:Configuration="Release"
